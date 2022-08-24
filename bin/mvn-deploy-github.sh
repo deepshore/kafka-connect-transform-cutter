5a60b9d71349de9ea94cb09459fae3720793f605
Selamanse <selamanse@scheinfrei.info>
2022-08-24 12:59:50 +0200

    Add build and deploy automation resources

---
+++ bin/mvn-deploy-github.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+MVN_SETTINGS_TEMPLATE_FILE=.mvn/settings.xml.tmpl
+MVN_SETTINGS_FILE=.mvn/settings.xml
+POM_FILE=pom.xml
+
+sed -e "s/GH_REPO/${GH_REPO}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+sed -e "s/GH_DEPLOY_USERNAME/${GH_DEPLOY_USERNAME}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+sed -e "s/GH_DEPLOY_TOKEN/${GH_DEPLOY_TOKEN}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+
+if [[ $GITHUB_REF_NAME =~ '^[0-9].*$' ]]; then
+  echo "creating release version ${GITHUB_REF_NAME#v}"
+  mvn -s ${MVN_SETTINGS_FILE} release:prepare -DreleaseVersion=${GITHUB_REF_NAME#v} || true
+fi
+
+mvn -s ${MVN_SETTINGS_FILE} --batch-mode deploy -Dlicense.skip=true
